7fae7e935fd6c972f47b997453e8e2d14483b477
Chris Camp <cpcamp2@gmail.com>
2017-11-03 11:11:23 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Penns IMDB clone</h1>
+        </header>
+        <p className="App-intro">
+          Introduction Text
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
